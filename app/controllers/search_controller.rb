bb63e12e230fcdb17effd9573cf1e2bb90bca6eb
Sabrina Robinson <sabrinarob@gmail.com>
2018-07-11 10:18:07 -0600

    Removed API key from search controller because that's bad

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,9 @@
 class SearchController < ApplicationController
   def index
+    binding.pry
     conn = Faraday.new(:url => 'https://api/alt-fuel-stations.gov') do |faraday|
+      faraday.headers['X-API-KEY'] = ENV['API_KEY']
+      faraday.adapter Faraday.default_adapter
+    end
   end
 end
