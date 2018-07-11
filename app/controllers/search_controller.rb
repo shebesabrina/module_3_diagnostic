802d25f7502b1f3916e715d59ce7678beedfbc76
Sabrina Robinson <sabrinarob@gmail.com>
2018-07-11 10:47:50 -0600

    Working in terminal to see what connection/response I can get

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,11 @@
 class SearchController < ApplicationController
   def index
-    binding.pry
-    conn = Faraday.new(:url => 'https://api/alt-fuel-stations.gov') do |faraday|
+    conn = Faraday.new(:url => 'https:/api/alt-fuel-stations.gov') do |faraday|
       faraday.headers['X-API-KEY'] = ENV['API_KEY']
       faraday.adapter Faraday.default_adapter
+
+      binding.pry
+      # response = JSON.parse(conn.get('/api/alt-fuel-stations/v1/nearest.format?parameters.json'))
     end
   end
 end
